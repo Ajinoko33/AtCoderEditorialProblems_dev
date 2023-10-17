3e65cf0e60e7a42bb8e52c47553e62ba58bc345c
Ajinoko33 <2022kyopr03@gmail.com>
2023-10-18 01:51:07 +0900

    テーブル定義追加，修正

--- config/detabase-definition-test.sql
+++ config/detabase-definition-test.sql
@@ -15,14 +15,20 @@ CREATE TABLE contests (
 DROP TABLE IF EXISTS problems;
 CREATE TABLE problems (
   id VARCHAR(255) NOT NULL,
-  problem_index VARCHAR(255) NOT NULL,
+  index_sort_num INTEGER NOT NULL,
   name VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL,
-  is_experimental BOOLEAN NOT NULL,
   difficulty INTEGER,
   contest_id VARCHAR(255) NOT NULL,
   writer_id INTEGER NOT NULL,
   PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS problem_index_sort;
+CREATE TABLE problem_index_sort (
+  sort_num INTEGER NOT NULL,
+  problem_index VARCHAR(255) NOT NULL,
+  PRIMARY KEY (sort_num)
 );
 
 INSERT INTO writers VALUES
@@ -34,6 +40,18 @@ INSERT INTO contests VALUES
 ('abc002', 234567),
 ('abc003', 345678);
 INSERT INTO problems VALUES
-('abc001_a', 'A', 'hello world', 'A. hello world', TRUE, 100, 'abc001', 1),
-('abc002_a', 'A', 'next world', 'A. next world', TRUE, 200, 'abc002', 2),
-('abc002_b', 'B', 'last world', 'B. last world', FALSE, 300, 'abc002', 3);
+('abc001_a', 1, 'hello world', 'A. hello world', 100, 'abc001', 1),
+('abc002_a', 1, 'next world', 'A. next world', 200, 'abc002', 2),
+('abc002_b', 2, 'last world', 'B. last world', 300, 'abc002', 3);
+INSERT INTO problem_index_sort VALUES
+(1, 'A'),
+(2, 'B'),
+(3, 'C'),
+(4, 'D'),
+(5, 'E'),
+(6, 'F'),
+(7, 'F2'),
+(8, 'G'),
+(9, 'H'),
+(10, 'Ex'),
+(99999, 'OTHER');
--- config/detabase-definition.sql
+++ config/detabase-definition.sql
@@ -15,12 +15,30 @@ CREATE TABLE contests (
 DROP TABLE IF EXISTS problems;
 CREATE TABLE problems (
   id VARCHAR(255) NOT NULL,
-  problem_index VARCHAR(255) NOT NULL,
+  index_sort_num INTEGER NOT NULL,
   name VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL,
-  is_experimental BOOLEAN NOT NULL,
   difficulty INTEGER,
   contest_id VARCHAR(255) NOT NULL,
   writer_id INTEGER NOT NULL,
   PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS problem_index_sort;
+CREATE TABLE problem_index_sort (
+  sort_num INTEGER NOT NULL,
+  problem_index VARCHAR(255) NOT NULL,
+  PRIMARY KEY (sort_num)
+);
+INSERT INTO problem_index_sort VALUES
+(1, 'A'),
+(2, 'B'),
+(3, 'C'),
+(4, 'D'),
+(5, 'E'),
+(6, 'F'),
+(7, 'F2'),
+(8, 'G'),
+(9, 'H'),
+(10, 'Ex'),
+(99999, 'OTHER');
