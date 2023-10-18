45ce43e08b5b66aa92fd6f5860fad2fc16426b9a
Ajinoko33 <2022kyopr03@gmail.com>
2023-10-18 22:35:12 +0900

    テーブル定義修正

--- config/detabase-definition-test.sql
+++ config/detabase-definition-test.sql
@@ -15,7 +15,7 @@ CREATE TABLE contests (
 DROP TABLE IF EXISTS problems;
 CREATE TABLE problems (
   id VARCHAR(255) NOT NULL,
-  index_sort_num INTEGER NOT NULL,
+  index_id INTEGER NOT NULL,
   name VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL,
   difficulty INTEGER,
@@ -24,11 +24,12 @@ CREATE TABLE problems (
   PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS problem_index_sort;
-CREATE TABLE problem_index_sort (
-  sort_num INTEGER NOT NULL,
+DROP TABLE IF EXISTS problem_index;
+CREATE TABLE problem_index (
+  id INTEGER NOT NULL,
   problem_index VARCHAR(255) NOT NULL,
-  PRIMARY KEY (sort_num)
+  sort_order INTEGER NOT NULL,
+  PRIMARY KEY (id)
 );
 
 INSERT INTO writers VALUES
@@ -43,15 +44,15 @@ INSERT INTO problems VALUES
 ('abc001_a', 1, 'hello world', 'A. hello world', 100, 'abc001', 1),
 ('abc002_a', 1, 'next world', 'A. next world', 200, 'abc002', 2),
 ('abc002_b', 2, 'last world', 'B. last world', 300, 'abc002', 3);
-INSERT INTO problem_index_sort VALUES
-(1, 'A'),
-(2, 'B'),
-(3, 'C'),
-(4, 'D'),
-(5, 'E'),
-(6, 'F'),
-(7, 'F2'),
-(8, 'G'),
-(9, 'H'),
-(10, 'Ex'),
-(99999, 'OTHER');
+INSERT INTO problem_index VALUES
+(1, 'A', 1),
+(2, 'B', 2),
+(3, 'C', 3),
+(4, 'D', 4),
+(5, 'E', 5),
+(6, 'F', 6),
+(7, 'F2', 7),
+(8, 'G', 8),
+(9, 'H', 9),
+(10, 'Ex', 10),
+(99999, 'OTHER', 99999);
