f9cb60dd7282622f21661de670ef0dde354b6e0f
Ajinoko33 <2022kyopr03@gmail.com>
2023-10-24 23:30:28 +0900

    lambda作成

---
+++ lambda/saveProblems.py
@@ -0,0 +1,121 @@
+import os
+
+from aws_lambda_powertools.utilities.validation import validator
+import psycopg2
+
+INBOUND_SCHEMA = {
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "array",
+    "items": {
+        "type": "object",
+        "required": ["problem_id", "contest_id", "start_epoch_second", "problem_index", "name", "title", "writer"],
+        "properties": {
+            "problem_id": {
+                "type": "string",
+                "maxLength": 255
+            },
+            "contest_id": {
+                "type": "string",
+                "maxLength": 255
+            },
+            "start_epoch_second": {
+                "type": "integer"
+            },
+            "problem_index": {
+                "type": "string",
+                "maxLength": 255
+            },
+            "name": {
+                "type": "string",
+                "maxLength": 255
+            },
+            "title": {
+                "type": "string",
+                "maxLength": 255
+            },
+            "difficulty": {
+                "type": "integer"
+            },
+            "writer": {
+                "type": "string",
+                "maxLength": 255
+            }
+        }
+    }
+}
+
+def get_dsn():
+    # DB接続情報を返す
+    HOST = os.environ["HOST"]
+    PORT = os.environ["PORT"]
+    DBNAME = os.environ["DBNAME"]
+    USER = os.environ["USER"]
+    PASSWORD = os.environ["PASSWORD"]
+    return f"host={HOST} port={PORT} dbname={DBNAME} user={USER} password={PASSWORD}"
+
+def insert_into_contests(contests, cur):
+    if len(contests.keys()) == 0:
+        return
+
+    # SQL作成
+    sql = "INSERT INTO contests (id, start_epoch_second) VALUES"
+    sql += ",".join(["(%s, %s)"] * len(contests))
+    
+    # テーブルへ登録
+    data = []
+    for id, ses in contests.items():
+        data.append(id)
+        data.append(ses)
+    cur.execute(sql, data)
+
+    return
+
+def insert_into_problems(problems, cur):
+    if len(problems) == 0:
+        return
+    
+    # SQL作成
+    sql = "INSERT INTO problems (id, index_id, name, title, difficulty, contest_id, writer) VALUES"
+    sql += ",".join(["(%s, (SELECT id FROM problem_index pi WHERE pi.problem_index = %s), %s, %s, %s, %s, %s)"] * len(problems))
+
+    # テーブルへ登録
+    data = []
+    for problem in problems:
+        data.append(problem["problem_id"])
+        data.append(problem["problem_index"])
+        data.append(problem["name"])
+        data.append(problem["title"])
+        data.append(problem["difficulty"])
+        data.append(problem["contest_id"])
+        data.append(problem["writer"])
+    cur.execute(sql, data)
+
+    return
+
+def insert_problems(problems, dsn):
+    # コンテストを洗い出す
+    contests = {}
+    for problem in problems:
+        if problem["contest_id"] not in contests:
+            contests[problem["contest_id"]] = problem["start_epoch_second"]
+
+    # 登録処理
+    with psycopg2.connect(dsn) as con:
+        with con.cursor() as cur:
+            # コンテストテーブルへ登録
+            insert_into_contests(contests, cur)
+
+            # 問題テーブルへ登録
+            insert_into_problems(problems, cur)
+
+    return
+
+@validator(inbound_schema=INBOUND_SCHEMA)
+def lambda_handler(event, context):
+    # DB接続情報を取得
+    dsn = get_dsn()
+
+    # 格納
+    insert_problems(event, dsn)
+
+    return 
