045f7e6ad45acb71e221eef7db697262799d671e
Ajinoko33 <2022kyopr03@gmail.com>
2023-12-21 22:30:09 +0900

    sorter実装

--- frontend/atcoder-writers-problems/src/app/_components/SearchResultTabChildren.tsx
+++ frontend/atcoder-writers-problems/src/app/_components/SearchResultTabChildren.tsx
@@ -3,6 +3,8 @@ import { Flex, Table } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { FC } from 'react';
 
+const DIFF_INF = 100000;
+
 export type SearchResultTabChildrenProps = {
   problems: Problem[];
 };
@@ -11,6 +13,8 @@ interface DataType {
   key: string;
   contest: string;
   title: string;
+  difficulty?: number;
+  sortOrder: number;
 }
 
 const columns: ColumnsType<DataType> = [
@@ -19,12 +23,30 @@ const columns: ColumnsType<DataType> = [
     dataIndex: 'contest',
     key: 'contest',
     width: 110,
+    sorter: (a, b) => {
+      if (a.contest > b.contest) return 1;
+      else if (a.contest < b.contest) return -1;
+      else return a.sortOrder - b.sortOrder;
+    },
+    sortDirections: ['ascend', 'descend', 'ascend'],
+    defaultSortOrder: 'descend',
+    showSorterTooltip: false,
   },
   {
     title: 'Title',
     dataIndex: 'title',
     key: 'title',
     ellipsis: true,
+  },
+  {
+    title: 'diff',
+    dataIndex: 'difficulty',
+    key: 'difficulty',
+    width: 80,
+    sorter: (a, b) => (a.difficulty || -DIFF_INF) - (b.difficulty || -DIFF_INF),
+    sortDirections: ['ascend', 'descend', 'ascend'],
+    showSorterTooltip: false,
+    render: (text) => text || '-',
   },
 ];
 
@@ -39,6 +61,8 @@ export const SearchResultTabChildren: FC<SearchResultTabChildrenProps> = ({
     key: idx.toString(),
     contest: problem.contest,
     title: problem.title,
+    difficulty: problem.difficulty,
+    sortOrder: problem.sortOrder,
   }));
 
   return (
