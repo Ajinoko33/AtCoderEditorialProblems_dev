83128d85b1bc844a87f4d2c4394195729547e008
Ajinoko33 <2022kyopr03@gmail.com>
2023-12-30 15:01:11 +0900

    GitHub Pagesへのデプロイのための設定

--- frontend/atcoder-editorial-problems/next.config.js
+++ frontend/atcoder-editorial-problems/next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const urlPrefix = '/AtCoderEditorialProblems';
+
 const nextConfig = {
   output: 'export',
   // reactStrictMode: false,
+  // assetPrefix: urlPrefix, 利用は非推奨
+
+  /* static export時にコメントを外す */
+  // basePath: urlPrefix,
+  // trailingSlash: true,
 };
 
 module.exports = nextConfig;
